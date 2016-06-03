b0fd021960bc54e437285c0fd967c4a991f238b0
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-02 23:57:04 -0400

    Fix typo in production config file

--- config/prod.exs
+++ config/prod.exs
@@ -15,9 +15,9 @@ config :taskmaster, Taskmaster.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "floating-taiga-54370.herokuapp.com", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
-  
+
 # Do not print debug messages in production
 config :logger, level: :info
 
